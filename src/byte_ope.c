39f3e342b8baa6649ef98bc01b8bac80eb310274
edillenb <elio.dillenberg@gmail.com>
2019-11-11 17:42:50 +0100

    reste a gerer le cas de la declaration inversee pour les direct 2 et ensuite il faudra gerer les indirects

--- src/byte_ope.c
+++ src/byte_ope.c
@@ -88,6 +88,23 @@ int generate_ocp(t_env *env)
   return (ocp);
 }
 
+int   looking_for_label(t_env *env, char *to_find)
+{
+  t_lst *current;
+
+  current = env->label;
+  while (current)
+  {
+    if (ft_strcmp(to_find, current->name) == 0)
+    {
+      printf("found the right label: %s\n", current->name);
+      return (current->type);
+    }
+    current = current->next;
+  }
+  return (0);
+}
+
 void	w_header(t_env *env)
 {
 	int		magic;
@@ -95,8 +112,6 @@ void	w_header(t_env *env)
 	char	comment[2052];
 	int		i;
   int size;
-
-
 
 	i = -1;
   printf("SIZE : %d\n", env->size);
@@ -137,19 +152,28 @@ void	w_header(t_env *env)
   int octet;
   int cpt_octet;
   int cpt_instr;
+  int label_index;
 
   cpt_octet = 0;
+  // cpt_octet == compteur de la taille totale depuis le PC
   while (env->list)
   {
     zap_all(env);
     if (!env->list)
       break ;
+    //octet == la taille totale de l'element que nous somme entrain d'analyser : peut etre == 1/2/4
     octet = get_size(env);
+    //on incremente la taille totale depuis le PC
     cpt_octet += octet;
+    // Si le type est une instruction valide ->
+    if (env->list->type == TYPE_INDEX)
+       printf("INDEX FOUND\n");
     if (env->list->type >= 1 && env->list->type <= 16)
     {
+      //on stock son index depuis CP dans cpt_instr
       cpt_instr = cpt_octet;
-      write(env->fd_cor, &(env->list)->type, octet);
+      //on ecrit l'instruction dans la memoire (toutjours sur 1 octet)
+      write(env->fd_cor, &(env->list)->type, 1);
       if (env->list->type != TYPE_INSTRUCTION_ZJMP && env->list->type != TYPE_INSTRUCTION_LIVE && env->list->type != TYPE_INSTRUCTION_FORK && env->list->type != TYPE_INSTRUCTION_LFORK)
       {
         ocp = generate_ocp(env);
@@ -160,8 +184,21 @@ void	w_header(t_env *env)
     {
       if (env->list->type == TYPE_DIRECT_2)
       {
+        //si on rencontre un direct_2 qui fait reference a un label
         if (env->list->name[1] == ':')
-          op = env->label->next->type - cpt_instr;
+        {
+          // on parcours grace a looking_for_label notre liste de label et on recupere l'index du label en question
+          label_index = looking_for_label(env, env->list->name + 2);
+          // if (label_index > cpt_instr)
+            op = label_index - cpt_instr;
+          // else
+          // {
+            // il s'agit ici de trouver une facon de recupere l'index correspondant quand le label a ete declare avant l'appel
+            //possible qu'on ait juste besoin de cast en unsigned short (ca devrait fonctionner, mais la si je change op en unsigned short, ca abort)
+            // op = 65535 - (env->size - (cpt_instr - label_index));
+          // }
+          printf("op = %d, env->list->name + 2 = %s, label_index = %d, cpt_instr = %d, size  = %d\n", op, env->list->name + 2, label_index, cpt_instr, env->size);
+        }
         else
           op = ft_atoi(&(env->list)->name[1]);
       }
