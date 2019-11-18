3670b10ee9adeaa277d921dcea6da5e3cea5d6b8
Thomas HALLOT <thallot@e1r8p5.42.fr>
2019-11-18 16:12:06 +0100

    void

--- vm/VM/zork.s
+++ vm/VM/zork.s
@@ -1,9 +1,7 @@
 .name "zork"
 .comment "I'M ALIIIIVE"
 
-l2:		sti r1, 11, %:live
-		and r1, %0, r6
-
-live:	live %1
-		fork %12
-		zjmp %:live
+l2: sti r1,%:live,%1
+and r1, %0, r1
+live: live %1
+zjmp %:live
